2d7253359f9931ff2a038bdb60b5327b4988b378
Shiju <nirmal.shiju23@bcp.org>
2022-10-07 10:31:25 -0700

    Fixed them dependencies

--- GeometryDash_ShijuGoyal.java
+++ GeometryDash_ShijuGoyal.java
@@ -1,3 +1,13 @@
-public class GeometryDash_ShijuGoyal {
-    
+import acm.program.GraphicsProgram;
+import acm.graphics.GRect;
+import acm.graphics.GLabel;
+
+public class GeometryDash_ShijuGoyal extends GraphicsProgram {
+    public static void main(String[] args) {
+        new GeometryDash_ShijuGoyal().start(args);
+    }
+
+    public void run() {
+        System.out.println("Hi");
+    }
 }
